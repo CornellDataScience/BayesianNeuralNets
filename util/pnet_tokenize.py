87cbf5d9056f64a59223fab7074bc5555f06c9a2
cameton <cai29@cornell.edu>
2018-04-24 04:35:36 -0400

    Made folders into packages, added a utility for parsing model specification files, began creating bnn framework, renamed files

---
+++ util/pnet_tokenize.py
@@ -0,0 +1,30 @@
+import re
+
+
+def tokenize_net(file):
+    f = open(file)
+    det = f.read()
+
+    ret = []
+
+    det = det.split("|")
+
+    for l in det:
+        ret.append(tokenize_layer(l))
+
+    return ret
+
+
+def tokenize_layer(l):
+    ret = {}
+    ins = re.split(r":", re.sub(r"[\n\s]", "", l))
+    layerdet = re.split(r"[\(\)]", ins[0])
+    ret["ltype"] = layerdet[0]
+    if len(layerdet) > 1 and not layerdet[1]:
+        ret["lspec"] = layerdet[1]
+
+    ret["act"] = ins[1]
+    ret["params"] = [i for i in map(float, re.split(r",", ins[2]))]
+    ret["dists"] = re.split(r",", ins[3])
+
+    return ret
